e1befd2d3077e2f9146f12c51ae845ed77e4caf0
Helaman <byhelaman@gmail.com>
2023-04-16 21:33:05 -0500

    general changes

---
+++ README.md
@@ -0,0 +1,74 @@
+# Challenge ONE - Build a text encryptor with Javascript.
+
+This is a solution to the [Build a text encryptor with Javascript](https://www.aluracursos.com/challenges/challenge-one-logica/sprint01-construye-un-encriptador-texto-con-javascript). A challenge proposed by [AluraLatam](https://www.aluracursos.com/), which seeks to improve coding skills by building an application that can encrypt and decrypt text messages.
+
+## Table of contents
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Preview](#preview)
+  - [Links](#links)
+- [The process](#the-process)
+  - [Requirements](#requirements)
+  - [Built with](#built-with)
+  - [Extras](#extras)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Welcome to our first challenge!
+
+In this challenge, you will build an application that will allow you to encrypt and decrypt texts. You will then be able to exchange secret messages with other people who know the cipher used.
+
+The encryption `keys` we will use are the following:
+
+```
+The letter "e" becomes "enter".
+The letter "i" becomes "imes".
+The letter "a" becomes "ai".
+The letter "o" becomes "ober".
+The letter "u" becomes "ufat".
+```
+
+### Preview
+![](./design/desktop-desing.png)
+\
+↪ *This is a preview of the challenge.*
+
+### Links
+
+- Solution URL: [Link](https://github.com/byhelaman/encryptor)
+- Live Site URL: [Link](https://byhelaman.github.io/encryptor/)
+
+## The process
+### Requirements
+- The application must only work with lowercase letters.
+- Letters with accents or special characters must not be used.
+- It must be possible to convert a word to the encrypted version and also return an encrypted word to its original version.
+
+For example:
+```
+"gato" => "gaitober"
+"gaitober" => "gato"
+```
+
+- The page must have fields to insert the text to be encrypted or decrypted, and the user must be able to choose between the two options.
+- The result should be displayed on the screen.
+
+### Built with
+
+- Semantic HTML5 markup
+- JavaScript logic
+- CSS custom properties
+- Flexbox | CSS Grid
+- Mobile-first workflow
+
+### Extras
+
+A button to copy the encrypted/decrypted text to the transfer section, i.e. with the same functionality as `Ctrl+C` or the `Copy` option in the application menu.
+
+## Author
+
+- AluraLatam - [@byhelaman](https://app.aluracursos.com/user/byhelaman)
+- Twitter - [@byhelaman](https://www.twitter.com/byhelaman)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // input
 const form = document.getElementById('form');
-const textarea = document.getElementById('textarea');
+const textareaInput = document.getElementById('textarea-input');
+const textareaOutput = document.getElementById('textarea-output');
 
 // output
 const output = document.getElementById('output');
@@ -11,16 +12,16 @@ const copyButton = document.getElementById('copy');
 const encryptButton = document.getElementById('encrypt');
 const decryptButton = document.getElementById('decrypt');
 
-
+// prevent submit
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 });
 
 // character assignment for encryption/decryption
 const keys = {
-  'a': 'ai',
   'e': 'enter',
   'i': 'imes',
+  'a': 'ai',
   'o': 'ober',
   'u': 'ufat',
 }
@@ -54,45 +55,74 @@ function decrypt(encryptedMessage) {
   return decryptedMessage;
 }
 
+// functions to display elements
+function displayErrorMessage(message) {
+  outputMessage.innerHTML = `
+  <div class="error">
+    <img src="./images/illustration.png" alt="Illustration"/>
+    <div class="error__text">
+      <h1>No message was found</h1>
+      <p>${message}</p>
+    </div>
+  </div>
+`;
+}
+
+function hideOutputElements() {
+  outputMessage.style.display = "block";
+  textareaOutput.style.display = "none";
+  copyButton.style.display = "none";
+  output.style.justifyContent = "center";
+}
+
+function showOutputElements() {
+  output.style.justifyContent = "space-between";
+  outputMessage.style.display = "none";
+  textareaOutput.style.display = "block";
+  copyButton.style.display = "block";
+}
+
 // function to validate user input
 function validateTextarea() {
-  const textareaValue = textarea.value.trim();
+  const textareaValue = textareaInput.value.trim();
+  const regex = /^[a-zA-Z\s]*$/;
 
   if (textareaValue === "") {
-    outputMessage.innerHTML = `
-      <div class="error">
-        <img src="./images/illustration.png" alt="Illustration"/>
-        <div class="error__text">
-          <h1>No message was found</h1>
-          <p>Enter the text you want to encrypt or decrypt.</p>
-        </div>
-      </div>
-    `;
-    copyButton.style.display = "none";
-    output.style.justifyContent = "center";
-
-  } else {
-    output.style.justifyContent = "space-between";
-    copyButton.style.display = "block";
+    displayErrorMessage('Enter the text you want to encrypt or decrypt.');
+    hideOutputElements();
+    return;
   }
+
+  if (!regex.test(textareaInput.value)) {
+    displayErrorMessage('Special characters and numbers are not allowed.');
+    hideOutputElements();
+    return;
+  }
+
+  showOutputElements();
 }
 
 // function that copies the encrypted/decrypted message (clipboard - new message) 
 copyButton.addEventListener('click', () => {
   navigator.clipboard.writeText(outputMessage.textContent);
-  textarea.value = outputMessage.textContent;
+  textareaInput.value = textareaOutput.value;
 });
 
 // encrypt the message
 encryptButton.addEventListener('click', () => {
-  outputMessage.innerHTML = `<p>${encrypt(textarea.value)}</p>`;
+  textareaOutput.innerHTML = encrypt(textareaInput.value);
   validateTextarea();
 });
 
 // decrypt the message
 decryptButton.addEventListener('click', () => {
-  outputMessage.innerHTML = `<p>${decrypt(textarea.value)}</p>`;
+  textareaOutput.innerHTML = decrypt(textareaInput.value);
   validateTextarea();
+});
+
+// function to convert characters to lower case
+textareaInput.addEventListener('keyup', () => {
+  textareaInput.value = textareaInput.value.toLowerCase();
 });
 
 validateTextarea();
